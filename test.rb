f8cacb54cfc0c5fb52f9b2fca3782ef5a5914420
Peter Graham <peterghm@gmail.com>
2017-04-06 01:47:26 -0400

    simplify colors

---
+++ test.rb
@@ -0,0 +1,26 @@
+require 'active_support'
+
+module AtomColors
+  class RubyExample
+    CONSTANT = /^[0-9]+ regex awesomes$/
+
+    attr_reader :colorscheme
+
+    # TODO: Lorem ipsum dolor sit amet
+    def initialize(attributes = {})
+      @colorscheme = attributes[:colorscheme]
+    end
+
+    def self.examples
+      ['string', :symbol, true, false, nil, 99.9, 1..2].each do |value|
+        puts "it appears that #{value.inspect} is a #{value.class}"
+      end
+
+      {:key1 => :value1, key2: 'value2'}.each do |key, value|
+        puts "the #{key.inspect} key has a value of #{value.inspect}"
+      end
+
+      %w[One Two Three].each { |number| puts number }
+    end
+  end
+end
